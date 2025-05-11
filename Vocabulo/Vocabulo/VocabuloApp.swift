18abcf498a70a04581efcfa7c8ff52d8af6e10c6
Pedro Franco <pedrofranco@Pedros-MacBook-Pro.local>
2025-05-11 14:19:28 -0300

    add project and gitignore

---
+++ Vocabulo/Vocabulo/VocabuloApp.swift
@@ -0,0 +1,17 @@
+//
+//  VocabuloApp.swift
+//  Vocabulo
+//
+//  Created by Pedro Franco on 11/05/25.
+//
+
+import SwiftUI
+
+@main
+struct VocabuloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
